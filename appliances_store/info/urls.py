be9abf58c3ce4d0ce665ff9d48e55f2fe08173ff
deadapathy <maks_vaskov@bk.ru>
2022-02-16 00:54:46 +0600

    create registr form

--- appliances_store/info/urls.py
+++ appliances_store/info/urls.py
@@ -1,10 +1,12 @@
+from atexit import register
 from django.urls import path
 from .views import ProjectInfo, contacts, home, team
+from . import views
 
 urlpatterns = [
     path('', home),
     path('ProjectInfo/', ProjectInfo),
     path('team/', team),
-    path('contacts/', contacts)
-
+    path('contacts/', contacts),
+    path('register/', views.register, name='register'),
 ]
--- appliances_store/info/views.py
+++ appliances_store/info/views.py
@@ -2,13 +2,18 @@ from email import header
 from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login
+from .forms import UserRegistrationForm
 
 def home(request):
     return render(request, 'info/index.html')
 
 def ProjectInfo(request):
     return render(request, 'info/infoProject.html')
+
+#def Registration(request):
+ #   return render(request, 'info/registration.html')
+
 
 def team(request):
     header = "Информация о команде"   
@@ -23,3 +28,18 @@ def contacts(request):
     time = {"worktime" : "24/7"}
     data = {"header": header, "user": user, "time": time}
     return render(request, 'info/contacts.html', context=data)
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'info/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'info/register.html', {'user_form': user_form})
