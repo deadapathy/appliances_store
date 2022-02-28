2ae4db713910b8013a389fdae3767cec6dd6cd46
deadapathy <maks_vaskov@bk.ru>
2022-03-01 02:54:42 +0600

    add products model

--- appliances_store/info/urls.py
+++ appliances_store/info/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('ProjectInfo/', ProjectInfo),
     path('team/', team),
     path('contacts/', contacts),
+    path(r'^', include('shop.urls', namespace='shop')),
+    path(r'^$', views.product_list, name='product_list'),
+    path(r'^(?P<category_slug>[-\w]+)/$',
+        views.product_list,
+        name='product_list_by_category'),
+    path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.product_detail,
+        name='product_detail'),
 ]
--- appliances_store/info/views.py
+++ appliances_store/info/views.py
@@ -3,7 +3,8 @@ from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 def home(request):
     return render(request, 'info/index.html')
@@ -29,3 +30,24 @@ def contacts(request):
     data = {"header": header, "user": user, "time": time}
     return render(request, 'info/contacts.html', context=data)
 
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
